89babcd5cb94be837ed3f5b5c6f1332a7edf33e2
Edwin Hernandez <edwinhern.16@gmail.com>
2021-07-30 16:53:04 +0000

    added simple testing

---
+++ tests/test_basic.py
@@ -0,0 +1,22 @@
+import unittest, sys
+
+sys.path.append('../Easy-Games') # imports python file from parent directory
+from main import app
+
+class BasicTests(unittest.TestCase):
+
+    # executed prior to each test
+    def setUp(self):
+        self.app = app.test_client()
+
+    ###############
+    #### tests ####
+    ###############
+
+    def test_main_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        
+        
+if __name__ == "__main__":
+    unittest.main()
