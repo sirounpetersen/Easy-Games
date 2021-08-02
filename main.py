f28fc8293d50b7486e3d809bccb749654d52c301
Godwin Silayo <godwinsilayo100@gmail.com>
2021-08-02 20:24:54 +0000

    updated website

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from flask import Flask, request, render_template, flash
-<<<<<<< HEAD
-from api import anticipated_games,the_most_popular,search,platform_games
-=======
 from api import anticipated_games, the_most_popular, search, platform_games
->>>>>>> 1d869aa421331924e7217ed96fab128135be4ab3
 import re
 
 
@@ -15,30 +11,22 @@ def cleanhtml(raw_html):
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '2d6ca153ea201fe4daf5a90f380026b5'
-<<<<<<< HEAD
-=======
 
->>>>>>> 1d869aa421331924e7217ed96fab128135be4ab3
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
 
-<<<<<<< HEAD
-@app.route("/home") 
-=======
-@app.route("/home")  # Why do we have this route too
->>>>>>> 1d869aa421331924e7217ed96fab128135be4ab3
+@app.route("/home")
 def home_2():
     return render_template("home.html")
 
 
 @app.route("/search", methods=['POST'])
 def home_post():
-<<<<<<< HEAD
 	name = request.form.get('name')
-	try:
+  try:
 		detail, image, rate, platform, platformRate,website = search(name)
 		if website == "":
 			website = "/notfound"
@@ -50,27 +38,11 @@ def home_post():
             flash(f'Your search keyword was not found. Try again!')
             return render_template("home.html")
 
-a,b = the_most_popular()
-@app.route("/trending") #Changing the navbar to trending
-=======
-    name = request.form.get('name')
-    try:
-        detail, image, rate, platform, platformRate, website = search(name)
-        return render_template("search.html", name=name.upper(),
-                               detail=cleanhtml(detail),
-                               image=image, rate=rate,
-                               website=website,
-                               both=zip(platform, platformRate))
-    except Exception:
-        flash('Your search keyword was not found. Try again!')
-        return render_template("home.html")
-
 
 a, b = the_most_popular()
 
 
-@app.route("/trending")  # Changing the navbar to trending
->>>>>>> 1d869aa421331924e7217ed96fab128135be4ab3
+@app.route("/trending") #Changing the navbar to trending
 def trending():
     global a
     global b
@@ -85,9 +57,9 @@ def platform():
 @app.route("/platformGames", methods=['POST'])
 def platform_post():
     platformName = request.form.get("console")
-    games = platform_games(platformName)
+    game2, gameplus = platform_games(platformName)
     return render_template('platformGames.html',
-                           console=platformName, games=games)
+                           console=platformName, games=zip(game2,gameplus))
 
 
 c, d = anticipated_games()
@@ -108,25 +80,19 @@ def about():
 def notfound():
 	return render_template("notfound.html")
 
-
-if __name__ == '__main__':
-<<<<<<< HEAD
-  app.run(debug=True, host="0.0.0.0")
-
-'''
-@app.route("/search/<keyword>", methods=['POST'])
+@app.route("/search/<keyword>")
 def search_name(keyword):
-	name = keyword
+	name = keyword.replace("+"," ")
 	#print(name)
 	try:
 		detail, image, rate, platform, platformRate,website = search(name)
 		return render_template("search.html", name=name.upper(), detail=cleanhtml(detail), 
 													 image=image, rate=rate, website=website, both=zip(platform,platformRate))
 	except:
-		return '<script>window.alert("Your search keyword was not found. Try again!"); window.open("/home")</script>'
-'''
+            flash(f'{name} was not found. Try again!')
+            return render_template("home.html")
 
 
-=======
-    app.run(debug=True, host="0.0.0.0")
->>>>>>> 1d869aa421331924e7217ed96fab128135be4ab3
+if __name__ == '__main__':
+  app.run(debug=True, host="0.0.0.0")
+
